e26d53087a1229434ead1db0077a7031870402b1
yamapi06150 <yamapi06150@gmail.com>
2017-07-04 17:49:29 +0800

    complete basic layout of frontend

--- index.js
+++ index.js
@@ -10,10 +10,15 @@ const webpackConfig = require('./webpack.config');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
 
 app.use(express.static(`${__dirname}/public`));// server static file
 app.use(webpackDevMiddleware(webpack(webpackConfig))); // for webpack setting
 app.use(bodyParser.urlencoded({ extend: true }));
+
+// socket-io set up
+const io = socketIo(server);
+io.on('connection', (socket) => {
+  console.log('made socket connection', socket.id);
+});
 
 server.listen(3000);
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -6,10 +6,20 @@ class App extends Component {
     super(props);
     this.state = { test: '' };
   }
+  componentDidMonunt = () => {
+    this.socket = io('/'); // connect to socket-io server
+
+  }
   render() {
     return (
-      <div>
+      <div id="mario-chat">
         <h1>socketio-practice-simple-chatroom</h1>
+        <div id="chat-window">
+          <div id="output"></div>
+        </div>
+        <input type="text" id="handl" placeholder="handle.."/>
+        <input type="text" id="message" placeholder="message.." />
+        <button id="send">Send</button>
       </div>
     );
   }
