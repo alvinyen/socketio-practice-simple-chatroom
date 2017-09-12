77776a80a200482c53f4bc192d5bfc64d7145f13
alvinyen <alvinyen219@gmail.com>
2017-09-12 18:13:23 +0800

    finish

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -8,7 +8,8 @@ class App extends Component {
       handle: '',
       message: '',
       messageData: [],
-      feedback: ''
+      feedback: '',
+      named: false,
      };
   }
   componentDidMount = () => {
@@ -28,6 +29,7 @@ class App extends Component {
         this.setState({handle: event.target.value});
         break;
       case 'message':
+        this.setState({feedback: ''});
         this.setState({message: event.target.value});
         break;
       default: 
@@ -37,45 +39,132 @@ class App extends Component {
     // console.log(`${event.target.name}: ${this.state[event.target.name]}`);
   }
   handleSendClick = (event) => {
-    
     this.socket.emit('chat', {
       message: this.state.message,
       handle: this.state.handle
     });
-    this.setState({ message: '', handle: '' });
+    this.setState({ message: '' });
+  }
+  handleUnLockClick = (event) => {
+    this.setState({ named: false });
+    if ( this.state.message === '' ) return;
+    this.socket.emit('chat', {
+      message: this.state.message,
+      handle: this.state.handle
+    });
+    this.setState({ message: '' });
   }
   handleFeedbackDivKeyPress = (event) => {
     console.log(this.state.handle);
     this.socket.emit('typing', this.state.handle);
   }
+  handleHandleKeyUp = (event) => {
+    if (event.keyCode === 13) {
+      this.setState({ named: true });
+    }
+  }
+
+  handleMessageKeyUp = (event) => {
+    if (event.keyCode === 13) {
+      if ( this.state.message === '' ) return;
+      this.socket.emit('chat', {
+        message: this.state.message,
+        handle: this.state.handle
+      });
+      this.setState({ message: '' });
+    }
+  }
+
+  getStyles = () => {
+    return {
+      title: {
+        width: '300px',
+        color: '#5162D2',
+        fontSize: '28px',
+        display: 'inline-block',
+        margin: '0 auto',
+      },
+      handle: {
+        borderTop: '1px solid #DDDDDD',
+      },
+      disabled: {
+        border: 'red 5px solid',
+      },
+      alignRight: {
+        textAlign: 'right',
+      },
+      alignLeft: {
+        textAlign: 'left',
+      },
+      messageBlock: {
+        wordWrap: 'break-word', 
+        wordBreak: 'break-all', 
+        overflow: 'hidden',
+
+        display: 'inline-block',
+        border: '1px solid #DDDDDD',
+        borderRadius: '1.3em',
+        maxWidth: '200px',
+        backgroundColor: '#008AF8',
+        padding: '10px',
+        color: 'white',
+        textAlign: 'justify',
+      }
+    };
+  }
+  getMessageBlock = (msg) => {
+      return <div style={this.getStyles().messageBlock}>{msg}</div>
+  }
   render() {
-    const { messageData } = this.state;
+    const { messageData, handle } = this.state;
+    const styles = this.getStyles();
+    
     const messageParagraph = messageData.map( (data, index) => {
-      return <p key={data + index}><strong>{`${data.handle}：`}</strong>{`${data.message}`}</p>;
+      const yourself = handle === data.handle;
+      return <p key={data + index} style={ yourself ? styles.alignRight : styles.alignLeft }>
+                {( yourself ? <div></div> : <strong>{`${data.handle}：`} </strong>)}  {this.getMessageBlock(data.message)}
+            </p>;
     });
     return (
       <div id="mario-chat">
-        <h1>socketio-practice-simple-chatroom</h1>
+        <div id="chat-title-container">
+          <div style={styles.title}>simple chat room</div>
+        </div>
+  
         <div id="chat-window">
           <div id="output">{messageParagraph}</div>
           <div id="feedback">{this.state.feedback}</div>
         </div>
-        <input 
-          type="text" 
-          id="handle" 
-          name="handle" 
-          placeholder="handle.."
-          value={this.state.handle}
-          onChange={this.handleChange} />
-        <input 
-          type="text" 
-          id="message" 
-          name="message" 
-          placeholder="message.."
-          value={this.state.message}
-          onChange={this.handleChange} 
-          onKeyPress={this.handleFeedbackDivKeyPress} />
-        <button id="send" onClick={this.handleSendClick}>Send</button>
+
+        <div style={ this.state.named ? styles.disabled: {} }>
+          <input 
+            style={styles.handle}
+            type="text" 
+            id="handle" 
+            name="handle" 
+            placeholder="name.."
+            value={this.state.handle}
+            onKeyUp={this.handleHandleKeyUp}
+            onChange={this.handleChange} 
+            disabled={this.state.named ? 'disabled' : ''}
+          />
+        </div>
+        { this.state.named ? <button id="unlock" onClick={this.handleUnLockClick}>Click to unLock.</button> : '' }
+        <div style={ !this.state.named ? styles.disabled: {} }>
+          <input 
+            type="text" 
+            id="message" 
+            name="message" 
+            placeholder="message.."
+            onKeyUp={this.handleMessageKeyUp}
+            value={this.state.message}
+            onChange={this.handleChange} 
+            onKeyPress={this.handleFeedbackDivKeyPress} 
+            disabled={ !this.state.named ? 'disabled' : ''}
+          />
+          {/* <button id="send" onClick={this.handleSendClick} disabled={ !this.state.named ? 'disabled' : ''} >Send</button> */}
+          { !this.state.named ? <button disabled id="unlock">Locked, pls enter your name above to unlock.</button> : '' }
+        </div>
       </div>
     );
   }
